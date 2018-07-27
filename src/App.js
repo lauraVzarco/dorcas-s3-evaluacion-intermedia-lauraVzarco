5d4625acc35d18b42ede8df11e64ccb6b13274be
Laura <laurav.zarco@gmail.com>
2018-07-27 17:00:41 +0200

    ejercicio planteado solo :(

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Target from './Target';
 
 class App extends Component {
   render() {
+    console.log(this.props.pokedata)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <div>
+          <Target/>
+        </div>
+        );
+      }
   }
-}
 
 export default App;
---
+++ src/Target.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Target extends Component {
+  render (){
+  return(
+    <ul>
+      <li>
+        <img className="pokemoImage" src="" alt="pokemon"/>
+        <p className="pokemonName">
+          Hola
+        </p>
+        <div className="pokemonType">
+          Hola
+        </div>
+      </li>
+    </ul>
+)
+  }
+}
+
+export default Target;
